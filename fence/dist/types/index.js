eba19a9b4eef99612057e9ef92adacf87be06db4
MunyinSam <kelvinsam233@gmail.com>
2026-05-01 19:45:22 +0700

    remove all files to start rewriting

--- fence/dist/types/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SkillLevel = void 0;
-var SkillLevel;
-(function (SkillLevel) {
-    SkillLevel[SkillLevel["Knows"] = 0] = "Knows";
-    SkillLevel[SkillLevel["Learning"] = 1] = "Learning"; // 1
-})(SkillLevel || (exports.SkillLevel = SkillLevel = {}));
-//# sourceMappingURL=index.js.map
